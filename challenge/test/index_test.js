052013c4762e0af14ea2c415bd00fe0c4b162519
Charles Dunn <dunnc1551@gmail.com>
2019-01-22 14:27:05 -0700

    Change var to const.

--- challenge/test/index_test.js
+++ challenge/test/index_test.js
@@ -3,7 +3,7 @@ const PokerHand = require('../index');
 
 describe('hand should be valid', function() {
   it('should hold 5 cards', function() {
-    var hand = new PokerHand('As Ks Qs Js 10s 2c');
+    const hand = new PokerHand('As Ks Qs Js 10s 2c');
     assert.equal(hand.rankHand(), 'Invalid Hand');
   });
 });
