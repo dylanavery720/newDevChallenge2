0691d1a6faeb9e62cc6f730383249ec311670292
Dylan A <dylanpatrickavery@gmail.com>
2019-02-02 10:22:28 -0700

    Finish challenge and tests

--- challenge/index.js
+++ challenge/index.js
@@ -1,7 +1,90 @@
+const deck = require('./constants/deck')
+
 class PokerHand {
-  constructor() {}
+  constructor(hand) {
+    this.hand = hand
+  }
+
+  sortArray(a, b) {
+    return a - b
+  }
+
   rankHand() {
-    return null;
+    let hand = this.convertCards(this.hand)
+    if (hand.values.length != 5 || hand.values.includes(undefined)) return 'Invalid Hand'
+    if(this.isFlush(hand)) return this.isFlush(hand)
+    if(this.isStraight(hand.values)) return this.isStraight(hand.values)
+    if(this.isFullHouse(hand)) return this.isFullHouse(hand)  
+    //Try to optimize so that method isn being called twice to return value
+  }
+
+  convertCards(hand) {
+    const cards = hand.split(" ")
+    let values = []
+    let suits = []
+    for(let i =0; i<=cards.length-1; i++) {
+      values.push(deck[cards[i].slice(0,1)])
+      suits.push(cards[i].slice(-1))
+    }
+    values.sort(this.sortArray)
+    return {values, suits}
+  }
+
+  isFullHouse(hand) {
+    let cards = this.countCards(hand.values)
+    let rank = "High Card"
+    for( let count in cards) {
+        if(cards[count] == 4) return "Four Of A Kind"
+        if(cards[count] == 3) {
+          rank = "Three Of A Kind"
+          if (Object.keys(cards).length < 3) rank = "Full House"
+        }
+        if(cards[count] == 2) {
+          rank = "One Pair"
+          if(Object.keys(cards).length < 4) rank = "Two Pair"
+        }
+    }
+    return rank
+  }
+
+  countCards(values) {
+    let handCount = {}
+    for (let i=0; i<=values.length -1; i++) {
+      handCount[values[i]] = handCount[values[i]] ? handCount[values[i]] +1 : 1
+    }
+    return handCount
+  }
+
+  isStraight(values) {
+    if(values.includes(2)) values = this.handleAce(values)
+    for(let i = values.length - 1; i >= 1; i--) {
+      if(values[i] == (values[i-1] +1)) {
+        continue
+      } else {
+        return false
+      }
+    }
+    return "Straight"
+  }
+
+  handleAce(values) {
+    values[values.indexOf(14)] = 1
+    return values.sort(this.sortArray)
+  }
+
+  isFlush(hand) {
+    let rank;
+    for(let i =hand.suits.length -1; i>=1; i--) {
+      if(hand.suits[i] == hand.suits[i-1]) {
+        continue
+      } else {
+        return false
+      }
+   }
+   rank = "Flush"
+   if(this.isStraight(hand.values)) rank = "Straight Flush"
+   if(hand.values[0] >= 10) rank = "Royal Flush"
+   return rank
   }
 }
 
--- challenge/test/index_test.js
+++ challenge/test/index_test.js
@@ -1,9 +1,75 @@
 const assert = require('assert');
 const PokerHand = require('../index');
 
-describe('hand should be valid', function() {
+describe('hand should be ranked properly', function() {
   it('should hold 5 cards', function() {
     const hand = new PokerHand('As Ks Qs Js 10s 2c');
     assert.equal(hand.rankHand(), 'Invalid Hand');
+  });
+
+  it('should not accept strange cards', function() {
+    const hand = new PokerHand('Ds Km Qs Fs 15s');
+    assert.equal(hand.rankHand(), 'Invalid Hand');
+  });
+
+  it('should rank a royal flush', function() {
+    const hand = new PokerHand('As Ks Qs Js 10s');
+    assert.equal(hand.rankHand(), 'Royal Flush');
+  });
+
+  it('should rank a straight flush', function() {
+    const hand = new PokerHand('Ks Qs Js 9s 10s');
+    assert.equal(hand.rankHand(), 'Straight Flush');
+  });
+
+  it('should rank a flush', function() {
+    const hand = new PokerHand('As 4s 3s 8s 10s');
+    assert.equal(hand.rankHand(), 'Flush');
+  });
+
+  it('should rank a straight', function() {
+    const hand = new PokerHand('6s 2d 3h 4h 5s');
+    assert.equal(hand.rankHand(), 'Straight');
+  });
+
+  it('should rank a straight with low ace', function() {
+    const hand = new PokerHand('As 2d 3h 4h 5s');
+    assert.equal(hand.rankHand(), 'Straight');
+  });
+
+  it('should rank a straight with high ace', function() {
+    const hand = new PokerHand('As Ks Qs Jh 10d');
+    assert.equal(hand.rankHand(), 'Straight');
+  });
+
+  it('should rank a four of a kind', function() {
+    const hand = new PokerHand('As 10c 10s 10h 10d');
+    //TODO: Error handling, should not be able to have two of the same card.
+    assert.equal(hand.rankHand(), "Four Of A Kind");
+  });
+
+  it('should rank a three of a kind', function() {
+    const hand = new PokerHand('As 10c 10s 10h 9d');
+    assert.equal(hand.rankHand(), "Three Of A Kind");
+  });
+
+  it('should rank a full house', function() {
+    const hand = new PokerHand('9s 10c 10s 10h 9d');
+    assert.equal(hand.rankHand(), "Full House");
+  });
+
+  it('should rank a two pair', function() {
+    const hand = new PokerHand('As 10c 10s 9h 9d');
+    assert.equal(hand.rankHand(), "Two Pair");
+  });
+
+  it('should rank a pair', function() {
+    const hand = new PokerHand('As 10c 10s 4h 9d');
+    assert.equal(hand.rankHand(), "One Pair");
+  });
+
+  it('should rank a high card', function() {
+    const hand = new PokerHand('As 10c 6s 4h 9d');
+    assert.equal(hand.rankHand(), "High Card");
   });
 });
